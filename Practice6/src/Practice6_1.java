c0a742781235cbbd73de6caba49bf457c7703d51
gemiJ <30407907+gemiJ@users.noreply.github.com>
2020-04-26 17:01:49 +0900

    Week6

--- Practice6/src/Practice6_1.java
+++ Practice6/src/Practice6_1.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Practice6_1 {
 
     public static void main(String[] args) {
 
+        double a,b,c,d,e,f;
+
+        System.out.print("a,b,c,d,e,f를 순서대로 입력하세요: ");
+
+        Scanner input = new Scanner(System.in);
+        a = input.nextDouble();
+        b = input.nextDouble();
+        c = input.nextDouble();
+        d = input.nextDouble();
+        e = input.nextDouble();
+        f = input.nextDouble();
+
+        LinearEquation le = new LinearEquation(a,b,c,d,e,f);
+
+        if(le.isSolvable()) {
+            System.out.print("X = " + le.getX() + ", Y = " + le.getY());
+        }else{
+            System.out.println("the equation has no solution");
+        }
+
     }
+
 }
